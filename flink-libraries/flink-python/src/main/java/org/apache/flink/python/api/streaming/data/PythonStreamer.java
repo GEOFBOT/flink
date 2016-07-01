b3371d60051ffc2053ae7dec72bf76ca719d607a
Geoffrey Mon <geofbot@gmail.com>
2016-06-30 22:43:16 -0400

    [FLINK-4098] Better comment placement

--- flink-libraries/flink-python/src/main/java/org/apache/flink/python/api/streaming/data/PythonStreamer.java
+++ flink-libraries/flink-python/src/main/java/org/apache/flink/python/api/streaming/data/PythonStreamer.java
@@ -254,7 +254,8 @@ public class PythonStreamer implements Serializable {
 						case SIGNAL_BUFFER_REQUEST:
 							if (i.hasNext() || sender.hasRemaining(0)) {
 								size = sender.sendBuffer(i, 0);
-								sendWriteNotification(size, sender.hasRemaining(0)); // i.hasNext() hangs for Python API iterations after sender.sendBuffer()
+								// Not sure why but i.hasNext() hangs after sender.sendBuffer() for Python iterations
+								sendWriteNotification(size, sender.hasRemaining(0));
 							} else {
 								throw new RuntimeException("External process requested data even though none is available.");
 							}
